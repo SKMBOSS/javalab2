b2ade7be071c0a20b905af2c844da8faa17d44d0
SKMBOSS <zzangjungsu@gmail.com>
2015-09-17 13:08:49 +0900

    20123404 Lee JungSu

---
+++ src/Pet.java
@@ -0,0 +1,104 @@
+
+public class Pet extends Animal
+{
+	private String name;
+	private int age;
+	private double weight;
+	
+	//오버라이딩
+	public String toString()
+	{
+		return ("Name :" + name + "\nAge : " + age +"years" +"\nWeight :" +weight + " pounds");
+	}
+	
+	public Pet(String initialName, int initialAge, double initialWeight, String iniGender)
+	{
+		super(iniGender);  // 부모 클래스 생성자 초기화
+
+		name = initialName;
+		
+		if(initialAge <0)
+		{
+			System.out.println("Error: Negative age.");
+			System.exit(0);
+		}
+		else
+			age = initialAge;
+		
+		weight = initialWeight;
+	}
+	
+	//오버로딩
+	public Pet(String initialName)
+	{
+		name = initialName;
+		age = 0;
+		weight =0;
+	}
+	
+	public Pet(int initialAge)
+	{
+		name = "No name yet.";
+		
+		weight = 0;
+		if(initialAge <0)
+		{
+			System.out.println("Error: Negative age.");
+			System.exit(0);
+		}
+		else
+			age = initialAge;
+	}
+
+	public Pet() 
+	{
+		name = "No name yet.";
+		age = 0;
+		weight = 0;
+	}
+	
+	public void set(String newName, int newAge, double newWeight) 
+	{
+		name = newName;
+		age = newAge;
+		weight = newWeight;
+	}
+	
+	public void setAge( int newAge) 
+	{
+		if(newAge <0)
+		{
+			System.out.println("Error: Negative age.");
+			System.exit(0);
+		}
+		else
+			age = newAge;
+	}
+	
+	public void setWeight( double newWeight)
+	{
+		if(newWeight <0)
+		{
+			System.out.println("Error: Negative weight.");
+			System.exit(0);
+		}
+		else
+		weight = newWeight;
+    }
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public int getAge() 
+	{
+		return age;
+	}
+	
+	public double getWeight()
+	{
+		return weight;
+	}
+
+}
